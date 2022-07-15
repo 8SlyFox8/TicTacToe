bdf30240a22b849c5d3376619c1e8f7790b0706f
8SlyFox8 <hitrinmaksim@mail.ru>
2022-07-15 12:42:42 +0300

    Добавлена возможность сохранять и считывать файл настроек для сетевой игры

--- config.cpp
+++ config.cpp
@@ -12,3 +12,70 @@ config::~config()
 {
     delete ui;
 }
+
+///
+/// \brief config::showEvent
+/// \param event
+/// Настройка полей ввода для окна настроек
+/// и считыванте настроей из файла настроек
+///
+void config::showEvent(QShowEvent *event)
+{
+    Q_UNUSED(event);
+
+    mask = QRegExp("[0-9.]{0,13}"); //Макска для ввода IP
+    ui->lineEdit->setValidator(new QRegExpValidator(mask));
+    //Макска для ввода порта
+    ui->lineEdit_2->setValidator(new QIntValidator(0, 99999, this));
+
+    file_config.setFileName("Settings/LANsettings.cfg");
+    if (file_config.open(QIODevice::ReadOnly))
+    {
+        IP = file_config.readLine();
+        IP = IP.left(IP.indexOf("\r\n"));
+        ui->lineEdit->setText(IP);
+        port = file_config.readLine();
+        ui->lineEdit_2->setText(port);
+        file_config.close();
+    }
+    else
+    {
+        QMessageBox::information(this, "Ошибка", "Файл настроек не загрузился");
+    }
+   void showEvent();
+}
+
+
+///
+/// \brief config::on_pushButton_clicked
+/// Сохраннение настроек IP и порта для сетевой игры в файл
+///
+void config::on_pushButton_clicked()
+{
+    //Проверка и/или создание папки настроек
+    if (!dir.exists("Settings"))
+    {
+        dir.mkdir("Settings");
+    }
+    file_config.setFileName("Settings/LANsettings.cfg");
+    if (file_config.open(QIODevice::WriteOnly))
+    {
+        QTextStream writeStream(&file_config);
+        QTextCodec *codec = QTextCodec::codecForName("utf8");
+        writeStream << ui->lineEdit->text() << codec->toUnicode("\r\n")
+                    << ui->lineEdit_2->text();
+        file_config.close();
+    }
+    else
+    {
+        QMessageBox::information(this, "Ошибка", "Файл настроек не cохранён");
+    }
+    config::close(); //Зарыть окно настроек
+}
+
+void config::on_pushButton_2_clicked()
+{
+    config::close(); //Зарыть окно настроек
+}
+
+
--- config.h
+++ config.h
@@ -2,6 +2,13 @@
 #define CONFIG_H
 
 #include <QDialog>
+#include <QRegExp>
+#include <QRegExpValidator>
+#include <QDir>
+#include <QFile>
+#include <QTextStream>
+#include <QTextCodec>
+#include <QMessageBox>
 
 namespace Ui {
 class config;
@@ -11,12 +18,25 @@ class config : public QDialog
 {
     Q_OBJECT
 
+protected:
+    void showEvent(QShowEvent *event) override;
+
 public:
     explicit config(QWidget *parent = nullptr);
     ~config();
 
+private slots:
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_clicked();
+
 private:
     Ui::config *ui;
+    QRegExp mask; //Макска для ввода IP
+    QDir dir; //Папка для настроек
+    QFile file_config; //Файл настроек
+    QString IP; //IP-адрес противника в сетивой игре
+    QString port; //Порт противника в сетивой игре
 };
 
 #endif // CONFIG_H
